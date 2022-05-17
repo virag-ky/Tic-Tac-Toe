0ae0c7b7460b4175712365015a45be0cbbfb53b0
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-17 19:54:59 +0300

    Add conditional statements

--- app.js
+++ app.js
@@ -1,100 +1,139 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  const player1Name = document.getElementById("player1");
-  const player2Name = document.getElementById("player2");
-  const markButtons = document.querySelector(".mark-buttons-div");
-  const submitPlayer = document.getElementById("submit-player1");
-  const playersDiv = document.querySelector(".players");
-  const player1Span = document.getElementById("player1-name");
-  const player2Span = document.getElementById("player2-name");
-  const buttonsXO = [...document.querySelectorAll(".mark-button")];
-  const blocks = [...document.getElementsByClassName("block")];
-  const startGameBtn = document.querySelector(".start-game");
+const form = document.querySelector("form");
+const player1Name = document.getElementById("player1");
+const player2Name = document.getElementById("player2");
+const markButtons = document.querySelector(".mark-buttons-div");
+const submitPlayer = document.getElementById("submit-player1");
+const playersDiv = document.querySelector(".players");
+const player1Span = document.getElementById("player1-name");
+const player2Span = document.getElementById("player2-name");
+const buttonsXO = [...document.querySelectorAll(".mark-button")];
+const blocks = [...document.getElementsByClassName("block")];
+const startGameBtn = document.querySelector(".start-game");
 
-  markButtons.style.display = "none";
-  playersDiv.style.display = "none";
+markButtons.style.display = "none";
+playersDiv.style.display = "none";
+startGameBtn.style.display = "none";
+player2Name.value = "computer";
+player2Name.setAttribute("readonly", "readonly");
+
+let player;
+let computer;
+let playerSelection;
+let computerSelection;
+
+submitPlayer.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.style.display = "none";
+  playersDiv.style.display = "block";
+  markButtons.style.display = "block";
+  player2Span.innerText = player2Name.value;
+  player1Span.innerText = player1Name.value;
+
+  player = player1Span.innerText;
+  computer = player2Span.innerText;
+});
+
+buttonsXO.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    playerSelection = e.target.innerText;
+    computerSelection = playerSelection == "X" ? "O" : "X";
+    markButtons.style.display = "none";
+    startGameBtn.style.display = "block";
+  })
+);
+
+let index;
+let winner;
+const tie = "It's a tie!";
+const arrayOfBlocks = ["", "", "", "", "", "", "", "", ""];
+
+startGameBtn.addEventListener("click", () => {
   startGameBtn.style.display = "none";
-  player2Name.value = "computer";
-  player2Name.setAttribute("readonly", "readonly");
 
-  let player;
-  let computer;
-  let playerSelection;
-  let computerSelection;
+  blocks.forEach((block) =>
+    block.addEventListener("click", () => {
+      block.innerText = playerSelection;
+      arrayOfBlocks[blocks.indexOf(block)] += playerSelection;
 
-  submitPlayer.addEventListener("click", (e) => {
-    e.preventDefault();
-    form.style.display = "none";
-    playersDiv.style.display = "block";
-    markButtons.style.display = "block";
-    player2Span.innerText = player2Name.value;
-    player1Span.innerText = player1Name.value;
+      let newBlocks = blocks.filter((block) => block.innerText === "");
+      index = Math.floor(Math.random() * newBlocks.length);
+      newBlocks[index].innerText = computerSelection;
 
-    player = player1Span.innerText;
-    computer = player2Span.innerText;
-  });
+      blocks.forEach((block) => {
+        if (block.innerText === computerSelection) {
+          arrayOfBlocks[blocks.indexOf(block)] = computerSelection;
+        }
+      });
 
-  buttonsXO.forEach((button) =>
-    button.addEventListener("click", (e) => {
-      playerSelection = e.target.innerText;
-      computerSelection = playerSelection == "X" ? "O" : "X";
-      markButtons.style.display = "none";
-      startGameBtn.style.display = "block";
+      arrayOfBlocks.every((item) => {
+        if (item !== "") {
+          winner = tie;
+        }
+      });
+      checkForWinningPatterns(arrayOfBlocks);
+      console.log(arrayOfBlocks, winner);
     })
   );
-
-  let index;
-  const arrayOfBlocks = ["", "", "", "", "", "", "", "", ""];
-
-  startGameBtn.addEventListener("click", () => {
-    startGameBtn.style.display = "none";
-
-    blocks.forEach((block, i) =>
-      block.addEventListener("click", () => {
-        block.innerText = playerSelection;
-        arrayOfBlocks[blocks.indexOf(block)] += playerSelection;
-
-        let newBlocks = blocks.filter((block) => block.innerText === "");
-        index = Math.floor(Math.random() * newBlocks.length);
-        newBlocks[index].innerText = computerSelection;
-
-        console.log(arrayOfBlocks);
-        checkForWinningPatterns(arrayOfBlocks);
-        console.log(winner);
-        if (block.innerText !== "") {
-          return;
-        }
-      })
-    );
-  });
-
-  let winner;
-
-  const checkForWinningPatterns = (array) => {
-    if (array[0] === "X" && array[1] === "X" && array[2] === "X") {
-      if (playerSelection === "X") winner = player;
-    } else {
-      winner = computer;
-    }
-    if (array[3] === "X" && array[4] === "X" && array[5] === "X") {
-      if (playerSelection === "X") winner = player;
-    } else {
-      winner = computer;
-    }
-    if (array[6] === "X" && array[7] === "X" && array[8] === "X") {
-      if (playerSelection === "X") winner = player;
-    } else {
-      winner = computer;
-    }
-    if (array[2] === "X" && array[4] === "X" && array[6] === "X") {
-      if (playerSelection === "X") winner = player;
-    } else {
-      winner = computer;
-    }
-    if (array[0] === "X" && array[4] === "X" && array[8] === "X") {
-      if (playerSelection === "X") winner = player;
-    } else {
-      winner = computer;
-    }
-  };
 });
+
+const checkForWinningPatterns = (array) => {
+  if (playerSelection === "X" && computerSelection === "O") {
+    if (
+      (array[0] === "X" && array[1] === "X" && array[2] === "X") ||
+      (array[3] === "X" && array[4] === "X" && array[5] === "X") ||
+      (array[6] === "X" && array[7] === "X" && array[8] === "X") ||
+      (array[0] === "X" && array[3] === "X" && array[6] === "X") ||
+      (array[1] === "X" && array[4] === "X" && array[7] === "X") ||
+      (array[2] === "X" && array[5] === "X" && array[8] === "X") ||
+      (array[2] === "X" && array[4] === "X" && array[6] === "X") ||
+      (array[0] === "X" && array[4] === "X" && array[8] === "X")
+    ) {
+      winner = player;
+    }
+  }
+
+  if (playerSelection === "O" && computerSelection === "X") {
+    if (
+      (array[0] === "O" && array[1] === "O" && array[2] === "O") ||
+      (array[3] === "O" && array[4] === "O" && array[5] === "O") ||
+      (array[6] === "O" && array[7] === "O" && array[8] === "O") ||
+      (array[0] === "O" && array[3] === "O" && array[6] === "O") ||
+      (array[1] === "O" && array[4] === "O" && array[7] === "O") ||
+      (array[2] === "O" && array[5] === "O" && array[8] === "O") ||
+      (array[2] === "O" && array[4] === "O" && array[6] === "O") ||
+      (array[0] === "O" && array[4] === "O" && array[8] === "O")
+    ) {
+      winner = player;
+    }
+  }
+
+  if (playerSelection === "O" && computerSelection === "X") {
+    if (
+      (array[0] === "X" && array[1] === "X" && array[2] === "X") ||
+      (array[3] === "X" && array[4] === "X" && array[5] === "X") ||
+      (array[6] === "X" && array[7] === "X" && array[8] === "X") ||
+      (array[0] === "X" && array[3] === "X" && array[6] === "X") ||
+      (array[1] === "X" && array[4] === "X" && array[7] === "X") ||
+      (array[2] === "X" && array[5] === "X" && array[8] === "X") ||
+      (array[2] === "X" && array[4] === "X" && array[6] === "X") ||
+      (array[0] === "X" && array[4] === "X" && array[8] === "X")
+    ) {
+      winner = computer;
+    }
+  }
+
+  if (playerSelection === "X" && computerSelection === "O") {
+    if (
+      (array[0] === "O" && array[1] === "O" && array[2] === "O") ||
+      (array[3] === "O" && array[4] === "O" && array[5] === "O") ||
+      (array[6] === "O" && array[7] === "O" && array[8] === "O") ||
+      (array[0] === "O" && array[3] === "O" && array[6] === "O") ||
+      (array[1] === "O" && array[4] === "O" && array[7] === "O") ||
+      (array[2] === "O" && array[5] === "O" && array[8] === "O") ||
+      (array[2] === "O" && array[4] === "O" && array[6] === "O") ||
+      (array[0] === "O" && array[4] === "O" && array[8] === "O")
+    ) {
+      winner = computer;
+    }
+  }
+};
